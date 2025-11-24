d374ee82e3a448eada4d3a0a73df270e9c844508
amanda093 <amandadarocha116@gmail.com>
2025-11-24 18:01:52 -0300

    feat: adicionei as imagens e coloquei gradient em alguns botôes
    Co-authored-by: Apolo <Chrb09@users.noreply.github.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       {/* Header */}
 
       {/* TODO: Liquid Glass */}
-      <header className="glass-header w-[calc(100%-2.7em)] rounded-[2em] py-[0.95em] m-[1.35em] fixed top-0 left-0 right-0 z-50">
+      <header className="glass-header w-[calc(100%-2.7em)] rounded-[1.5em] py-[0.95em] m-[1.35em] fixed top-0 left-0 right-0 z-50">
         <nav className="container flex flex-row justify-between items-center">
           <div className="w-[20%]">
             <Image
@@ -18,7 +18,7 @@ export default function Home() {
               height={70}
             />
           </div>
-          <div className="flex justify-center items-center gap-[1em] w-[60%]">
+          <div className="flex justify-center items-center gap-[1.5em] w-[60%]">
             <Link href="#section-sonoris" className="text-header">
               Sonoris
             </Link>
@@ -46,11 +46,10 @@ export default function Home() {
       >
         <div className="flex flex-col items-center">
           {/* TODO: Gradiente nos textos*/}
-          <h1 className="text-[6em]! tracking-[0.125em] text-white font-semibold!">
+          <h1 className="text-[6em]! tracking-[0.125em] font-bold! text-transparent bg-linear-to-r from-[#FFFFFF] via-[#E0F9FF] to-[#FBFBFB] inline-block bg-clip-text">
             Sonoris
           </h1>
           <h2 className="text-center text-[2.5em]! leading-[1.1em] text-white font-medium!">
-            {" "}
             Transformando acessibilidade <br />
             em produtividade
           </h2>
@@ -58,22 +57,28 @@ export default function Home() {
       </section>
 
       <main className="px-[2em] pt-[5em] flex flex-col gap-[5em]">
+        {/* Transcriber Section */}
         <section
           id="section-transcriber"
           className="bg-white p-[2em] rounded-[1em]"
         >
           <div>
-            <h1 className="">Transcritor da Sonoris</h1>
+            <h1 className="text-transparent bg-linear-to-r from-[#142E50] from-10% via-[#1D5765] to-[#142E50] to-90% inline-block bg-clip-text">
+              Transcritor da Sonoris
+            </h1>
             <p>Dispositivo integrado que faz o trabalho para você.</p>
           </div>
         </section>
 
+        {/* App Section */}
         <section
           id="section-app"
-          className="flex bg-linear-to-t from-[#2890C4] to-[#327BF3] p-[2em] rounded-[1em] "
+          className="flex justify-center bg-linear-to-t from-[#2890C4] to-[#327BF3] p-[2em] rounded-[1em] "
         >
           <div>
-            <h1>Aplicativo da Sonoris</h1>
+            <h1 className=" text-transparent bg-linear-to-r from-[#FFFFFF] via-[#E0F9FF] to-[#FBFBFB] inline-block bg-clip-text">
+              Aplicativo da Sonoris
+            </h1>
             <p>A ferramenta que faz a diferença.</p>
 
             <div>
@@ -85,7 +90,7 @@ export default function Home() {
           </div>
 
           <Image
-            src="/logo-sonoris.png"
+            src="/aplicativo.png"
             alt="Description"
             width={500}
             height={140}
