61704a12a3480636bf9dabf3c79d75e523ea0f90
amanda093 <amandadarocha116@gmail.com>
2025-11-24 14:39:01 -0300

    feat: começo do header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,23 +4,56 @@ export default function Home() {
   return (
     <div className="bg-background">
       <main className="">
-        <section className="bg-linear-to-t from-[#28B4C4] to-[#5898FF] p-4 rounded-lg mb-8">
-          {/* Header */}
-          <header className="bg-white opacity-90 rounded-lg">
+        {/* Header */}
+        <header className="header-glass w-[calc(100% - 2.7em)] rounded-[2em] py-[0.95em] m-[1.35em] fixed top-0 left-0 right-0 z-10">
+          <nav className="container">
             <Image
               src="/logo-sonoris.png"
               alt="Description"
               width={252}
               height={70}
             />
-          </header>
-
+          </nav>
+        </header>
+        {/* Hero Section */}
+        <section className="bg-linear-to-t from-[#28B4C4] to-[#5898FF] h-[56em]">
           <h1>Sonoris</h1>
         </section>
-
-        <section>
-          <h1>Hello world</h1>
-        </section>
+        <h1>
+          <br />a
+          <br />a
+          <br />a
+          <br />a
+          <br />a
+          <br />a
+          <br />a
+          <br />a
+          <br />a
+          <br />a
+          <br />a
+          <br />a
+          <br />a
+          <br />a
+          <br />a
+          <br />a
+          <br />a
+          <br />a v
+          <br />a
+          <br />a
+          <br />a
+          <br />a
+          <br />a
+          <br />a v
+          <br />a
+          <br />a
+          <br />a v
+          <br />a
+          <br />a
+          <br />a
+          <br />a
+          <br />a
+          <br />a v
+        </h1>
       </main>
     </div>
   );
