b2e7e7a65483622d6eafac6d1374d28ac1d23f57
Amanda093 <vovigadagamer@gmail.com>
2025-11-24 10:54:53 -0300

    feat: adicionei a imagem e comecei a codar o header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,26 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="bg-white dark:bg-black sm:items-start">
-        <h1>Hello world</h1>
+    <div className="bg-background">
+      <main className="">
+        <section className="bg-linear-to-t from-[#28B4C4] to-[#5898FF] p-4 rounded-lg mb-8">
+          {/* Header */}
+          <header className="bg-white opacity-90 rounded-lg">
+            <Image
+              src="/logo-sonoris.png"
+              alt="Description"
+              width={252}
+              height={70}
+            />
+          </header>
+
+          <h1>Sonoris</h1>
+        </section>
+
+        <section>
+          <h1>Hello world</h1>
+        </section>
       </main>
     </div>
   );
